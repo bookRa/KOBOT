03a1329bc6fe2feba91cf7ddacfab5c110a48dbc
bookRa <ommarab@proton.me>
2023-04-05 16:10:45 -0700

    ridin the wave 🌊

--- kobot.py
+++ kobot.py
@@ -1,9 +1,23 @@
+import ccxt
 import os
 import openai
 from dotenv import load_dotenv
+import json 
 
 load_dotenv()
 
 openai.organization = "org-VAzYa2yfTTttUMNvCBhluUKn"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.Model.list())
+# print(openai.Model.list())
+
+# instantiate ccxt connector to kucoin exchange and get orderbook data
+exchange = ccxt.kucoin()
+exchange.load_markets()
+
+TICKERS_WE_LIKE = [
+    'BTC/USDT', 
+    'ADA/USDT' ]
+
+all_my_tickers = exchange.fetch_tickers(TICKERS_WE_LIKE)
+
+print(json.dumps(all_my_tickers, indent=4))
