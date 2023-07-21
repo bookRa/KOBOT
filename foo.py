7af2090db48b05d737ab52241c6be7020918526d
bookra <omar.abdelbadie1@gmail.com>
2023-07-21 19:59:42 -0400

    I'm back on the wagon

--- foo.py
+++ foo.py
@@ -1,9 +1,18 @@
-import ccxt
+import yaml
+
+with open("pmm_config_vars.yml") as f:
+    data = yaml.safe_load(f)
+    promptables = {k: v for k, v in data.items() if v["required"] and v["promptable"]}
+    the_rest = {k: v for k, v in data.items() if v["required"] and not v["promptable"]}
 
 
-exchange = ccxt.kucoin()
-exchange.load_markets()
+# print("promptables")
+# print(promptables)
+print("the_rest")
+print(the_rest)
 
-ticker_info = exchange.fetch_ticker("ADA/USDT")
-
-print(ticker_info)
+# with open("promptables.yml", "w") as f:
+#     reqs = {k: v["default"] for k, v in promptables.items()} | {
+#         k: v["default"] for k, v in the_rest.items()
+#     }
+#     yaml.dump(reqs, f)
