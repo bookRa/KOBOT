0cbd4cab8b1bf73ee728916cbcd7559002fc55b4
bookra <omar.abdelbadie1@gmail.com>
2023-07-27 00:35:35 -0400

    it's lit!

--- lit.py
+++ lit.py
@@ -11,95 +11,106 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-kc = ccxt.kucoin()
-kc.load_markets()
+# Constants TODO: make these user inputs
+COIN = "BTC/USDT"
+EXCHANGE_ID = "kucoin"
+OHLCV_TIMEFRAME = "5m"
 
-BTC = "BTC/USDT"
+# fetch data from CCXT
+exchange = eval(f"ccxt.{EXCHANGE_ID}()")
+exchange.load_markets()
+ticker_data = exchange.fetch_ticker(COIN)
+orderbook_data = exchange.fetch_order_book(COIN)
+ohlcv_data = exchange.fetch_ohlcv(COIN, timeframe=OHLCV_TIMEFRAME)
 
-# setup ticker data and plot
-ticker_data = kc.fetch_ticker(BTC)
+# organize ticker data
 ticker_data.pop("info")
 ticker_df = pd.DataFrame.from_dict([ticker_data])
 
-# display the ticker df in streamlit
-st.title("Kucoin Data")
-st.write("Ticker Data")
-st.write(ticker_df)
-
-# setup orderbook data and depth plot
-orderbook_data = kc.fetch_order_book(BTC)
+# organize orderbook data
 bids = orderbook_data["bids"]
 asks = orderbook_data["asks"]
-n = 20
-# "zoom in" on the orderbook data to show the bid/ask spread and the closest n bids/asks
+n = 20  # "zoom in" on the closest n bids/asks
 bids = bids[:n]
 asks = asks[:n]
 bids_df = pd.DataFrame(bids, columns=["Price", "Amount"])
-bids_df["Type"] = "bid"
 asks_df = pd.DataFrame(asks, columns=["Price", "Amount"])
-asks_df["Type"] = "ask"
 orderbook_df = pd.concat([bids_df, asks_df])
-
-# calculate the midpoint and the bid/ask spread (as a percentage with two significant digits)
 midpoint = (bids[0][0] + asks[0][0]) / 2
 spread = (asks[0][0] - bids[0][0]) / midpoint * 100
 
-fig, ax = plt.subplots()
-ax.set_title(f"{BTC} Orderbook ({n} best bids/asks)")
-sns.ecdfplot(
-    x="Price",
-    weights="Amount",
-    stat="count",
-    complementary=True,
-    color="green",
-    data=bids_df,
-    ax=ax,
+# organize OHLCV data
+ohlcv_df = pd.DataFrame(
+    ohlcv_data, columns=["Timestamp", "Open", "High", "Low", "Close", "Volume"]
 )
-sns.ecdfplot(
-    x="Price", weights="Amount", stat="count", color="red", data=asks_df, ax=ax
-)
-
-# annotate the ecdfplot with the midpoint and spread
-ax.axvline(midpoint, color="black", linestyle="--")
-ax.annotate(
-    f"Midpoint: {midpoint:.2f}\nSpread: {spread}%",
-    xy=(midpoint, 0.5),
-    xytext=(midpoint + 0.1, 3.5),
-    xycoords="data",
-    textcoords="data",
-    arrowprops=dict(arrowstyle="->", connectionstyle="arc3"),
-)
-
-# display the depth plot in streamlit
-st.write("Orderbook Data")
-st.pyplot(fig)
+ohlcv_df["Timestamp"] = pd.to_datetime(ohlcv_df["Timestamp"], unit="ms")
+ohlcv_df.set_index("Timestamp", inplace=True)
 
 
-# ohlcv_data = kc.fetch_ohlcv(BTC)
+# layout the streamlit app
+st.set_page_config(layout="wide")
+st.sidebar.title("Abacus (alpha)")
+st.sidebar.write(f"Exchange: {EXCHANGE_ID.capitalize()}")
+st.sidebar.write(f"Market: {COIN}")
+st.sidebar.button("Refresh Data")
+c1, c2 = st.columns([1, 1])
 
-# # Create exchange object
-# exchange = ccxt.kucoin()
-# exchange.load_markets()
+with c1:
+    c11, c12, c13 = st.columns([1, 1, 1])
+    # display the ticker, bid, and ask dataframes
+    with c11:
+        st.write("Ticker Data")
+        st.write(ticker_df.T)
+    with c12:
+        st.write(f"top {n} bids")
+        st.write(bids_df)
+    with c13:
+        st.write(f"top {n} asks")
+        st.write(asks_df)
 
-# # Create ticker dataframe
-# ticker_info = exchange.fetch_ticker("ADA/USDT")
-# ticker_df = pd.DataFrame(ticker_info)
+    # plot orderbook depth chart
+    fig, ax = plt.subplots()
+    ax.set_title(f"{COIN} Orderbook ({n} best bids/asks)")
+    sns.ecdfplot(
+        x="Price",
+        weights="Amount",
+        stat="count",
+        complementary=True,
+        color="green",
+        data=bids_df,
+        ax=ax,
+    )
+    sns.ecdfplot(
+        x="Price", weights="Amount", stat="count", color="red", data=asks_df, ax=ax
+    )
 
-# # Create orderbook dataframe
-# orderbook = exchange.fetch_order_book("ADA/USDT")
-# orderbook_df = pd.DataFrame(orderbook)
+    # annotate the ecdfplot with the midpoint and spread
+    ax.axvline(midpoint, color="black", linestyle="--")
+    annotation_y = max(sum(bids_df["Amount"]), sum(asks_df["Amount"])) * 0.85
+    ax.annotate(
+        f"Midpoint: {midpoint:.2f}\nSpread: {spread}%",
+        xy=(midpoint, annotation_y),
+        xytext=(midpoint + 0.1, annotation_y),
+        xycoords="data",
+        textcoords="data",
+    )
 
-# # Create OHLCV dataframe
-# ohlcv = exchange.fetch_ohlcv("ADA/USDT")
-# ohlcv_df = pd.DataFrame(
-#     ohlcv, columns=["Timestamp", "Open", "High", "Low", "Close", "Volume"]
-# )
+    # display the depth plot
+    st.write("Orderbook Depth Chart")
+    st.pyplot(fig)
 
-# # Create streamlit app
-# st.title("Kucoin Data")
-# st.write("Ticker Data")
-# st.write(ticker_df)
-# st.write("Orderbook Data")
-# st.write(orderbook_df)
-# st.write("OHLCV Data")
-# st.write(ohlcv_df)
+    # OHLCV plot
+    st.write(f"Open-High-Low-Close-Volume Candles ({OHLCV_TIMEFRAME})")
+    candlefig, candleax = mpf.plot(
+        ohlcv_df,
+        type="candle",
+        mav=5,
+        volume=True,
+        style="yahoo",
+        returnfig=True,
+    )
+    st.write(candlefig)
+
+    # OHLCV dataframe (optional)
+    with st.expander("Show OHLCV Data"):
+        st.write(ohlcv_df)
